ec70e0f8abff291e9c123ee595b921c1d91eb5cd
MonikaSty <mostyx@gmail.com>
2019-12-06 20:20:46 +0100

    excuser context update

--- Excuser/WebApplication1/DbContext/ExcuserContext.cs
+++ Excuser/WebApplication1/DbContext/ExcuserContext.cs
@@ -155,19 +155,19 @@ namespace WebApplication1.DbContext
 					{
 						Id = 2,
 						Value = "late",
-						
+
 					},
 					new Keyword
 					{
 						Id = 3,
 						Value = "friend",
-						
+
 					},
 					new Keyword
 					{
 						Id = 4,
 						Value = "home",
-						
+
 					},
 					new Keyword
 					{
@@ -219,24 +219,6 @@ namespace WebApplication1.DbContext
 					Id = 1,
 					Body = "Hi%name%! It looks like I might be delayed. There is a traffic jam, it seems there are some construction workers on the way. I will be there as soon as this moves!",
 					Tone = Tone.Friendly,
-					//ExcuseKeywords = new List<ExcuseKeyword>
-					//{
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 1,
-					//		KeywordId = 1
-					//	},
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 1,
-					//		KeywordId = 2
-					//	},
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 1,
-					//		KeywordId = 3
-					//	},
-					//},
 					SubcategoryId = 1
 				},
 				new Excuse
@@ -244,48 +226,12 @@ namespace WebApplication1.DbContext
 					Id = 2,
 					Body = "I apologize for the inconvinience, but I am unable to reach the time we have agreed. There seems to be a traffic jam ahead. I am unsure about how long will it take, but I will do my best to be with you as soon as I can. ",
 					Tone = Tone.Polite,
-					//ExcuseKeywords = new List<ExcuseKeyword>
-					//{
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 2,
-					//		KeywordId = 1
-					//	},
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 2,
-					//		KeywordId = 2
-					//	},
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 2,
-					//		KeywordId = 3
-					//	},
-					//},
 					SubcategoryId = 1
 				}, new Excuse
 				{
 					Id = 3,
 					Body = "Hey%name%! Sorry I’ll be a little late. I wasn’t expecting this but at home my roomies dog had fun with toilet paper and it is everywhere. I’ll take care of it and then meet you ASAP.",
 					Tone = Tone.Friendly,
-					//ExcuseKeywords = new List<ExcuseKeyword>
-					//{
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 3,
-					//		KeywordId = 2
-					//	},
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 3,
-					//		KeywordId = 5
-					//	},
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 3,
-					//		KeywordId = 4
-					//	},
-					//},
 					SubcategoryId = 2
 				},
 				new Excuse
@@ -293,24 +239,6 @@ namespace WebApplication1.DbContext
 					Id = 4,
 					Body = "Hello%name%! I apologise for being late. There was an urgent situation at home that need to be dealt with. Thank you for being patient with me and I’ll be there in 15 min.",
 					Tone = Tone.Polite,
-					//ExcuseKeywords = new List<ExcuseKeyword>
-					//{
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 4,
-					//		KeywordId = 2
-					//	},
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 4,
-					//		KeywordId = 5
-					//	},
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 4,
-					//		KeywordId = 4
-					//	},
-					//},
 					SubcategoryId = 2
 				},
 				new Excuse
@@ -318,48 +246,12 @@ namespace WebApplication1.DbContext
 					Id = 5,
 					Body = "Hi%name%. I got carried away while watching the Gossip Girls yesterday. My alarm was not strong enough to wake me up, so I will be a little late for our meetup! Hope that's fine, I will pay for the coffee!",
 					Tone = Tone.Friendly,
-					//ExcuseKeywords = new List<ExcuseKeyword>
-					//{
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 5,
-					//		KeywordId = 6
-					//	},
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 5,
-					//		KeywordId = 2
-					//	},
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 5,
-					//		KeywordId = 7
-					//	},
-					//},
 					SubcategoryId = 2
 				}, new Excuse
 				{
 					Id = 6,
 					Body = "Hello%name%! I have to apologize for being late for our meeting. I wasn't feeling well and over slept. I feel much better today, thank you!",
 					Tone = Tone.Polite,
-					//ExcuseKeywords = new List<ExcuseKeyword>
-					//{
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 6,
-					//		KeywordId = 6
-					//	},
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 6,
-					//		KeywordId = 2
-					//	},
-					//	new ExcuseKeyword
-					//	{
-					//		ExcuseId = 6,
-					//		KeywordId = 7
-					//	},
-					//},
 					SubcategoryId = 2
 				});
 
